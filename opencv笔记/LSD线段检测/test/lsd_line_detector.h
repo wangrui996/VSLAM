778e5b404d6c9129eafe28deee6219a93219136a
wangrui957 <wangrui957@163.com>
2021-10-06 18:18:15 +0800

    add lsd_line

---
+++ opencv笔记/LSD线段检测/test/lsd_line_detector.h
@@ -0,0 +1,35 @@
+/*
+
+
+
+
+
+*/
+
+
+
+#include <opencv/cv.h>
+
+
+namespace GS_SLAM
+{
+
+/**
+ * @brief LSD线段提取器
+ * @details 用于前端跟踪时从静态语义对象上提取。
+ */
+class LsdDeteector
+{
+private:
+    /* data */
+public:
+
+    LsdDe(/* args */);
+    ~LsdDe();
+};
+
+
+}   //namespace GS_SLAM
+
+
+
